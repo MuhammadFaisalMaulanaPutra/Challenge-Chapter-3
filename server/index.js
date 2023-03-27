82bea55978b52c9e35b1dc9e920229882199bd20
MuhammadFaisalMaulanaPutra <90740593+MuhammadFaisalMaulanaPutra@users.noreply.github.com>
2023-03-27 07:58:56 +0700

    banyak bgt

--- server/index.js
+++ server/index.js
@@ -1,1 +1,19 @@
-console.log("Implement servermu disini yak 😝!");
+const express = require("express");
+const app = express();
+const path = require("path");
+const PATH = path.join(__dirname, "../public/");
+const PORT = 8000;
+
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(PATH, "landing-page.html"));
+});
+
+app.get("/cars", (req, res) => {
+  res.sendFile(path.join(PATH, "search-cars.html"));
+});
+
+app.listen(PORT, () => {
+  console.log(`Server sudah berjalan, silahkan buka http://localhost:${PORT}`);
+});
