25e8d5df17826c890e1997a16075fb7ac183dc9c
MuhammadFaisalMaulanaPutra <90740593+MuhammadFaisalMaulanaPutra@users.noreply.github.com>
2023-03-31 15:52:50 +0700

    done (again again)

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -2,17 +2,27 @@ function search() {
   let driverType = document.getElementById("driverType");
   if (driverType.value === "1") {
     driverType = true;
-  } else {
+  } else if (driverType.value === "2") {
     driverType = false;
+  } else {
+    driverType = null;
   }
   let date = document.getElementById("datepicker");
-  date = date.value.split(" ");
-  date = date[2];
+  if (date.value == "") {
+    date = null;
+  } else {
+    date = date.value.split(" ");
+    date = date[2];
+  }
   let passengers = document.getElementById("passengers");
 
   clear();
 
-  filter(driverType, date, passengers);
+  if (driverType == null && date == null && passengers == null) {
+    alert("noParam");
+  } else {
+    filter(driverType, date, passengers);
+  }
 }
 
 function filter(driverType, date, passengers) {
@@ -24,8 +34,10 @@ function filter(driverType, date, passengers) {
       const car = res;
       let carsResult;
       carsResult = car.filter((car) => car.available === driverType);
-      carsResult = carsResult.filter((car) => car.year.toString() == date);
-      carsResult = carsResult.filter((car) => car.capacity >= passengers);
+      carsResult = carsResult.filter((car) => car.year.toString() === date);
+      console.log(carsResult);
+      carsResult = carsResult.filter((car) => car.capacity > passengers);
+      console.log(carsResult);
 
       upData(carsResult);
     });
@@ -33,11 +45,23 @@ function filter(driverType, date, passengers) {
 
 function upData(carsResult) {
   let container = document.getElementById("cars-container");
+  if (carsResult.length === 0) {
+    alert("noData");
+  }
   for (let index = 0; index < carsResult.length; index++) {
     const node = document.createElement("div");
     node.className = "col-md-4";
     node.innerHTML = render(carsResult[index]);
     container.appendChild(node);
+  }
+}
+
+function alert(message) {
+  let container = document.getElementById("cars-container");
+  if (message === "noParam") {
+    container.innerHTML = noParam();
+  } else if (message === "noData") {
+    container.innerHTML = noData();
   }
 }
 
@@ -49,6 +73,31 @@ function clear() {
     child.remove();
     child = container.firstElementChild;
   }
+}
+
+function noParam() {
+  return `
+          <div class="col-md-5">
+            <div class="alert alert-danger mt-5 text-center" role="alert">
+              <h1><i class="bi bi-exclamation-diamond"></i></h1>
+              <h4 class="alert-heading text-bold">Mobil Tidak Bisa Dicari</h4>
+              <hr />
+              <h5 class="mb-0">Silahkan Isi Kolom Untuk Menemukan Mobil Yang Dicari</h5>
+            </div>
+          </div>
+  `;
+}
+
+function noData() {
+  return `
+          <div class="col-md-5">
+            <div class="alert alert-danger mt-5 text-center" role="alert">
+              <h4 class="alert-heading text-bold">Mohon Maaf</h4>
+              <hr />
+              <h5 class="mb-0">Tidak Ada Mobil Yang Tersedia</h5>
+            </div>
+          </div>
+  `;
 }
 
 function render(data) {
